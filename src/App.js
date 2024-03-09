47ea7120517d26d76c70cccb2b5447a22f7565d5
akshayaca <akshayaca@outlook.com>
2024-03-08 19:53:26 -0500

    df

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JSONViewer from './JSONViewer';
+
+function App() {
+    return (
+        <div className="App">
+            <JSONViewer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Import your CSS file if you have one
+import App from './App'; // Assuming your main component is named App
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
